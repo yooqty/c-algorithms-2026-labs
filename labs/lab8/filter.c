fac6f4c573b69b6b916b8fb21e97d5a2309027fe
yooqty <yooqtty@gmail.com>
2026-06-12 22:14:32 +1000

    lab8 filter.c

---
+++ labs/lab8/filter.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int n;
+	double y[20], f[20];
+	 int k[7][19] = {
+		 { 1,2,1},
+     /// Р—РґРµСЃСЊ С‚Р°Р±Р»РёС†Р° РєРѕСЌС„С„РёС†РёРµРЅС‚РѕРІ
+	 }
+     ///  Р—РґРµСЃСЊ Р’Р°С€ РєРѕРґ
+     
+	return 0;
+}
